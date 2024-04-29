1605084fbb66c7495dfb8ea96c450309f8969bdd
naamp <nando.amport@gmail.com>
2024-04-29 21:35:14 +0200

    20240429_1_Nando
    Transferhistory:
    -Map
    -Drop Down
    -fetchTransferLines

--- client/src/transferhistory.js
+++ client/src/transferhistory.js
@@ -1,7 +1,16 @@
 // transferhistory (verantwortlich: Nando)
 // Inhalt: historymap, toolbar, ...
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import OSM from 'ol/source/OSM.js';
+import VectorLayer from 'ol/layer/Vector.js';
+import VectorSource from 'ol/source/Vector.js';
+import { fromLonLat } from 'ol/proj';
+import GeoJSON from 'ol/format/GeoJSON.js';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
@@ -17,12 +26,95 @@ import appbarstyle from './appbarstyle.js';
 import { useNavigate } from "react-router-dom";
 
 const Transferhistory = () => {
-  const [player, setPlayer] = React.useState('Max Meyer');
+  const [player, setPlayer] = useState();
+  const [players, setPlayers] = useState([]);
+  const [transferLines, setTransferLines] = useState([]);
+  const [map, setMap] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  const fetchPlayers = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typename=vw_spielerdaten&outputFormat=application/json');
+
+      if (response && response.data && response.data.features) {
+        const playerData = response.data.features.map(feature => feature.properties.spieler_name);
+        setPlayers(playerData);
+      } else {
+        console.error('No player data found in the response:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching players:', error);
+    }
+  };
 
   const handleChange = (event) => {
     setPlayer(event.target.value);
   };
+
+  const fetchTransferLines = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typename=vw_transferlinien&outputFormat=application/json');
+
+      if (response && response.data && response.data.features) {
+        const transferLinesData = response.data.features.map(feature => ({
+          id: feature.properties.id,
+          vonClubName: feature.properties.von_club_name,
+          nachClubName: feature.properties.nach_club_name,
+          spielerName: feature.properties.spieler_name,
+          vonClub: feature.properties.von_club,
+          nachClub: feature.properties.nach_club,
+          linie: feature.geometry
+        }));
+        setTransferLines(transferLinesData);
+      } else {
+        console.error('No transfer lines data found in the response:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching transfer lines:', error);
+    }
+  };
+
+  const initializeMap = () => {
+    const vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: transferLines.map(transferLine => {
+          const geojsonFormat = new GeoJSON();
+          return geojsonFormat.readFeature(transferLine.linie);
+        })
+      })
+    });
+
+    const newMap = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        vectorLayer // VectorLayer für transferLines hinzufügen
+      ],
+      view: new View({
+        center: fromLonLat([8.1, 46.9]),
+        zoom: 8
+      }),
+      target: 'map'
+    });
+
+    setMap(newMap);
+  };
+
+
+  useEffect(() => {
+    fetchTransferLines();
+  }, []);
+
+  useEffect(() => {
+    if (transferLines.length > 0) {
+      initializeMap();
+    }
+  }, [transferLines]);
 
   return (
     <div>
@@ -48,8 +140,9 @@ const Transferhistory = () => {
                   onChange={handleChange}
                   style={{ color: 'white' }}
                 >
-                  <MenuItem value="Max Meyer">Max Meyer</MenuItem>
-                  <MenuItem value="Martin Frydek">Martin Frydek</MenuItem>
+                  {players.map(player => (
+                    <MenuItem key={player} value={player}>{player}</MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </div>
@@ -59,6 +152,7 @@ const Transferhistory = () => {
           </div>
         </Toolbar>
       </AppBar>
+      <div id="map" className="map_transferhistory"></div> {/* Hier wird die Karte gerendert */}
     </div>
   );
 };
